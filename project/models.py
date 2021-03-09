d9e3cbc72aada70432890a6851cc1852528f05d4
Woshington Valdeci de Sousa <woshington.valdeci@ifsertao-pe.edu.br>
2021-03-09 17:42:05 -0300

    modificando o model user para não utilizar campos desnecessários e criando os models na app project

---
+++ project/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from accounts.models import User
+
+# Create your models here.
+
+class Project(models.Model):
+    id = models.AutoField(primary_key=True, db_column='idProjeto')
+    code = models.CharField(
+        "codigo", max_length=20, null=False, db_column='cdProjeto'
+    )
+    name = models.CharField(
+        "nome", max_length=80, null=False, db_column='nmProjeto'
+    )
+    description = models.TextField(
+        "descrição", null=True, db_column='deProjeto'
+    )
+    created_at = models.DateTimeField(
+        'criado em', auto_now_add=True
+    )
+    approval_date = models.DateTimeField(
+        "data aprovação", null=True, db_column='nmProjeto'
+    )
+    cancellation_date = models.DateTimeField("data aprovação", null=True)
+    status = models.PositiveSmallIntegerField(
+        "status", null=False, db_column="flStatus"
+    )
+    user = models.ForeignKey(
+        User, verbose_name="usuario", db_column="nuUsuario"
+    )
+
+    class Meta:
+        verbose_name = "Projeto"
+        verbose_name_plural = "Projetos"
+        db_table = "eptfProjeto"
+
+
+class Management(models.Model):
+    id = models.AutoField(primary_key=True, db_column='idGerenciamento')
+    budget = models.DecimalField(
+        "orçamento", null=False, max_digits=9, decimal_places=2, 
+        db_column="vlOrcamento"
+    )
+    spent = models.DecimalField(
+        "Gasto", null=False, max_digits=9, decimal_places=2, null=True,
+        db_column="vlGasto"
+    )
+    project = models.ForeignKey(
+        Project, verbose_name="projeto", db_column="nuProjeto"
+    )
+
+    class Meta:
+        verbose_name = "Gerenciamento"
+        verbose_name_plural = "Gerenciamentos"
+        db_table = "egstGerenciamento"
--- wf5/settings.py
+++ wf5/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
+    'project.apps.ProjectConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Fortaleza'
 
 USE_I18N = True
 
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'accounts.User'
